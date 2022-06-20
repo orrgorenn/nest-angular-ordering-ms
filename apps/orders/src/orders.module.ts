4a9ec84751b34526612796cb7d74b9e41fd9ec89
Orr Goren <orr.goren@zoominfo.com>
2022-06-20 23:55:57 +0300

    chore(orders): added order repo and updated module imports

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -4,6 +4,9 @@ import { OrdersController } from './orders.controller';
 import * as Joi from 'joi';
 import { OrdersService } from './orders.service';
 import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { DatabaseModule } from '@app/common';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
