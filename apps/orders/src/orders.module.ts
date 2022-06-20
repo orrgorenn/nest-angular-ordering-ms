ad3754ff7f9523cfb51efa8da86820ad93aa8f32
Orr Goren <orr.goren@zoominfo.com>
2022-06-20 23:35:32 +0300

    chore(order_module): connection to db

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
+import * as Joi from 'joi';
 import { OrdersService } from './orders.service';
+import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
