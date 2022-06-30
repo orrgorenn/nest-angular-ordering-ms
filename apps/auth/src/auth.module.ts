da7babdd343ca51470b32ca2830d55847e523b51
Orr Goren <orr.goren@zoominfo.com>
2022-06-30 19:38:24 +0300

    chore: added auth app

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule, RmqModule } from '@app/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from 'joi';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    RmqModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/auth/.env',
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configSerivce: ConfigService) => ({
+        secret: configSerivce.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configSerivce.get('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
